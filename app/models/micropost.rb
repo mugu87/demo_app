3a2449dc472e7b8e9dbb2c6ee6815ea3255a6d47
ntran2 <ntran2@uno.edu>
2013-10-05 12:35:54 -0500

    sublime commit

---
+++ app/models/micropost.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Micropost < ActiveRecord::Base
+	validates :content, length: {maximum: 140}
+end
